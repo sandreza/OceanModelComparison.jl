a80bbaa55db4ddac069644be6979bf9cf8f63c6a
sandreza <andrenogueirasouza@gmail.com>
2021-02-03 17:47:46 -0500

    general unstable bickley jet

---
+++ unstable_bickley/general/boiler_plate.jl
@@ -0,0 +1,25 @@
+using Test
+using JLD2
+using ClimateMachine
+ClimateMachine.init()
+using ClimateMachine.ODESolvers
+using ClimateMachine.Mesh.Filters
+using ClimateMachine.VariableTemplates
+using ClimateMachine.Mesh.Grids
+using ClimateMachine.Ocean
+
+include("TwoDimensionalCompressibleNavierStokesEquations.jl")
+
+using ClimateMachine.Mesh.Topologies
+using ClimateMachine.Mesh.Grids
+using ClimateMachine.DGMethods
+using ClimateMachine.BalanceLaws:
+    vars_state, Prognostic, Auxiliary, number_states
+using ClimateMachine.DGMethods.NumericalFluxes
+using ClimateMachine.MPIStateArrays
+using ClimateMachine.VTK
+
+using MPI
+using LinearAlgebra
+using StaticArrays
+using Logging, Printf, Dates
