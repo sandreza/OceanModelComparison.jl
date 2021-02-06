b5d793e15f9a89a1752b33e8568095c847237f92
sandreza <andrenogueirasouza@gmail.com>
2021-02-06 13:33:23 -0500

    adding parameter sweep

---
+++ three_dimensional/convenience.jl
@@ -0,0 +1,59 @@
+function generate_name(DOF, N, Nover, flux, periodic; L = 4π, mpicomm = MPI.COMM_WORLD, endtime = 200)
+    Ne = round(Int, DOF / (N+1))
+    Nˣ = Ne
+    Nʸ = Ne
+    Nᶻ = 1
+    Lˣ = L 
+    Lʸ = L
+    Lᶻ = L
+    # grid
+    xrange = range(-Lˣ / 2; length = Nˣ + 1, stop = Lˣ / 2)
+    yrange = range(-Lʸ / 2; length = Nʸ + 1, stop = Lʸ / 2)
+    zrange = range(-Lᶻ / 2; length = Nᶻ + 1, stop = Lᶻ / 2)
+    mpicomm = mpicomm
+    brickrange = (xrange, yrange, zrange)
+    topl = BrickTopology(
+        mpicomm,
+        brickrange,
+        periodicity = (true, periodic, true),
+        boundary = ((0, 0), (0, 0), (0,0)),
+    )
+    grid = DiscontinuousSpectralElementGrid(
+        topl,
+        FloatType = FT,
+        DeviceArray = Array,
+        polynomialorder = N + Nover,
+    )
+    
+    Δx =  min_node_distance(grid)
+    cfl = 0.15
+    dt = cfl * Δx / √10
+
+    # run
+    timeend = FT(endtime) # s
+    nout = round(Int, 2 / dt)
+    dt = 2 / nout
+    filename = "3Dflux_" * strip(string(flux), ['(', ')']) * "_p" * string(N) * "_N" * string(Ne) * "_Nover" * string(Nover) * "_periodicity_" * string(periodic)
+    f = jldopen(filename * ".jld2", "a+")
+    f["grid"] = grid
+    close(f)
+    return filename, Ne, dt
+end
+
+function just_generate_name(DOF, N, Nover, flux, periodic; L = 4π, mpicomm = MPI.COMM_WORLD, endtime = 200)
+    Ne = round(Int, DOF / (N+1))
+    Nˣ = Ne
+    Nʸ = Ne
+    Lˣ = L 
+    Lʸ = L
+    filename = "3Dflux_" * strip(string(flux), ['(', ')']) * "_p" * string(N) * "_N" * string(Ne) * "_Nover" * string(Nover) * "_periodicity_" * string(periodic)
+    return filename
+end
+
+function nameprettifier(name)
+    tmp = split(name, "_")
+    dof = "polynomial order " * tmp[3][2] * " and " * tmp[4][2:end] * " elements"
+    stringflux = ", a " * tmp[2] 
+    intorder = ", and integration order 2(" * tmp[3][2] * "+" * tmp[5][end] * ")- 1," 
+    return dof * stringflux * intorder
+end
--- three_dimensional/run_bickley_jet.jl
+++ three_dimensional/run_bickley_jet.jl
@@ -8,24 +8,37 @@ FT = Float64
 # RUN THE TESTS #
 #################
 
+# Domain size
+Lˣ = 4 * FT(π)  # m
+Lʸ = 4 * FT(π)  # m
+Lᶻ = 4 * FT(π)  # m
+
 vtkpath = abspath(joinpath(ClimateMachine.Settings.output_dir, "vtk_bickley_3D"))
 
-let
+DOFs = [32]
+Ns = [1, 2, 3, 4]
+Novers = [0, 1]
+fluxes = [RoeNumericalFlux(), RusanovNumericalFlux()]
+periodicity = [false, true]
+
+##
+DOFs = [32]
+Ns = [5]
+Novers = [0]
+fluxes = [RoeNumericalFlux()]
+periodicity = [true]
+
+for DOF in DOFs, N in Ns, Nover in Novers, flux in fluxes, periodic in periodicity
     # simulation times
     timeend = FT(200) # s
-    dt = FT(0.02) # s
-    nout = Int(100)
+    nout = 100
+
+    filename, Ne, dt = generate_name(DOF, N, Nover, flux, periodic)
 
     # Domain Resolution
-    N = 3
-    Nˣ = 8
-    Nʸ = 8
+    Nˣ = Ne
+    Nʸ = Ne
     Nᶻ = 1
-
-    # Domain size
-    Lˣ = 4 * FT(π)  # m
-    Lʸ = 4 * FT(π)  # m
-    Lᶻ = 4 * FT(π)  # m
 
     # model params
     cₛ = sqrt(10) # m/s
@@ -38,27 +51,47 @@ let
     domain = (; Lˣ, Lʸ, Lᶻ)
     timespan = (; dt, nout, timeend)
     params = (; cₛ, ρₒ, μ, ν, κ)
-
-    config = Config(
-        "rusanov_overintegration",
-        resolution,
-        domain,
-        params;
-        numerical_flux_first_order = RusanovNumericalFlux(),
-        Nover = 0,
-        periodicity = (true, true, true),
-        boundary = ((0, 0), (0, 0), (0, 0)),
-        boundary_conditons = (ClimateMachine.Ocean.OceanBC(
-            Impenetrable(FreeSlip()),
-            Insulating(),
-        ),),
-    )
-
+    if periodic
+        config = Config(
+            filename,
+            resolution,
+            domain,
+            params;
+            numerical_flux_first_order = flux,
+            Nover = Nover,
+            periodicity = (true, true, true),
+            boundary = ((0, 0), (0, 0), (0, 0)),
+            boundary_conditons = (ClimateMachine.Ocean.OceanBC(
+                Impenetrable(FreeSlip()),
+                Insulating(),
+            ),),
+        )
+    else
+        config = Config(
+            filename,
+            resolution,
+            domain,
+            params;
+            numerical_flux_first_order = flux,
+            Nover = Nover,
+            periodicity = (true, true, true),
+            boundary = ((0, 0), (1, 1), (0, 0)),
+            boundary_conditons = (ClimateMachine.Ocean.OceanBC(
+                Impenetrable(FreeSlip()),
+                Insulating(),
+            ),),
+        )
+    end
     tic = Base.time()
 
-    run_CNSE(config, resolution, timespan; TimeStepper = LSRK54CarpenterKennedy)
-
+    run_CNSE(config, resolution, timespan; TimeStepper = SSPRK22Heuns)
+    # LSRK54CarpenterKennedy
     toc = Base.time()
     time = toc - tic
-    println(time)
+
+    f = jldopen(filename * ".jld2", "a+")
+    f["simulationtime"] = time
+    f["threads"] = Threads.nthreads()
+    f["arraytype"] = string(ClimateMachine.array_type())
+    println(time, " seconds")
 end
