399ddc91ddd297c045ecddf0c7c7593fbd914868
sandreza <andrenogueirasouza@gmail.com>
2021-02-09 22:11:17 -0500

    add 3D movie

---
+++ three_dimensional/3Dtimeviz.jl
@@ -0,0 +1,80 @@
+include(pwd() * "/three_dimensional/convenience.jl")
+include(pwd() * "/unstable_bickley/periodic/imperohooks.jl")
+include(pwd() * "/unstable_bickley/periodic/vizinanigans.jl")
+
+DOFs = [64]
+Ns = [3]
+Novers = [1]
+fluxes = [RusanovNumericalFlux(), RoeNumericalFlux()]
+periodicity = [true] # [true, false]
+
+states = []
+namelist = []
+for DOF in DOFs, Nover in Novers, flux in fluxes, periodic in periodicity, N in Ns
+    name = just_generate_name(DOF, N, Nover, flux, periodic)
+    push!(namelist, name)
+    print(name)
+    println(" ")
+end
+##
+for name in namelist
+    f = jldopen(name * ".jld2")
+    simtime = f["simulationtime"]
+    threadnum = f["threads"]
+    array = f["arraytype"]
+    if array == "Array" 
+        archstring = " on the CPU with " * string(threadnum) * " threads"
+    else
+        archstring = " on the GPU"
+    end
+    prettyname = nameprettifier(name)
+    println("The simulation time was " * @sprintf("%0.2f", simtime) * " seconds for " * prettyname * archstring)
+    println("------------------------------")
+    # get old grid
+    newgrid = f["grid"]
+    gridhelper = GridHelper(newgrid)  
+    x, y, z = coordinates(newgrid)
+    ϕ =  ScalarField(copy(x), gridhelper)
+    # new grid
+    newx = range(-2π, 2π, length = DOFs[1])
+    newy = range(-2π, 2π, length = DOFs[1])
+    newz = range(-2π, 2π, length = DOFs[1])
+    ρθ = zeros(length(newx), length(newy), length(newz), 100)
+    # interpolate
+    M = view(newgrid.vgeo, :, newgrid.Mid, :)
+    for i in 1:100
+        Q = f[string(i)]
+        ϕ .= Q[:,5,:]
+        ρθ[:,:,:,i] = ϕ(newx, newy, newz, threads = true)
+    end
+    println("the mean value is ", sum(M .* ϕ.data))
+    val = sum(M .* ϕ.data)/sum(M .* abs.(ϕ.data))
+    if val ≥ eps(1e0)
+        println(name * " not conserved to machine precision at " * string(100))
+        println("the relative value is ", val)
+        println("the value is ", sum(M .* ϕ.data))
+    end
+    push!(states, ρθ)
+    close(f)
+end
+##
+vizstates = [states[2][:,:,:,i] for i in 1:100]
+scene = volumeslicetime(vizstates, aspect = (1,1,1))
+
+
+##
+seconds = 10
+fps = 10
+frames = round(Int, fps * seconds )
+record(scene, pwd() * "/3Dbickley.mp4"; framerate = fps) do io
+    for i = 1:frames
+        sleep(1/fps)
+        recordframe!(io)
+    end
+end
+##
+record(scene, "64dof.mp4", 1:100, framerate=10) do n
+    time_node[] = n
+    θ = -0.005 * 2π
+    rotate_cam!(scene.children[1], (θ, 0, 0))
+end
---
+++ three_dimensional/convenience.jl
@@ -0,0 +1,89 @@
+using Test
+using JLD2
+using ClimateMachine
+ClimateMachine.init(disable_gpu = true)
+using ClimateMachine.ODESolvers
+using ClimateMachine.Mesh.Filters
+using ClimateMachine.VariableTemplates
+using ClimateMachine.Mesh.Grids: polynomialorders
+using ClimateMachine.Ocean
+
+using ClimateMachine.Mesh.Topologies
+using ClimateMachine.Mesh.Grids
+using ClimateMachine.DGMethods
+using ClimateMachine.BalanceLaws:
+    vars_state, Prognostic, Auxiliary, number_states
+using ClimateMachine.DGMethods.NumericalFluxes
+using ClimateMachine.MPIStateArrays
+using ClimateMachine.VTK
+
+using MPI
+using LinearAlgebra
+using StaticArrays
+using Logging, Printf, Dates
+
+function generate_name(DOF, N, Nover, flux, periodic; L = 4π, mpicomm = MPI.COMM_WORLD, endtime = 200)
+    Ne = round(Int, DOF / (N+1))
+    Nˣ = Ne
+    Nʸ = Ne
+    Nᶻ = Ne
+    Lˣ = L 
+    Lʸ = L
+    Lᶻ = L
+    # grid
+    xrange = range(-Lˣ / 2; length = Nˣ + 1, stop = Lˣ / 2)
+    yrange = range(-Lʸ / 2; length = Nʸ + 1, stop = Lʸ / 2)
+    zrange = range(-Lᶻ / 2; length = Nᶻ + 1, stop = Lᶻ / 2)
+    mpicomm = mpicomm
+    brickrange = (xrange, yrange, zrange)
+    topl = BrickTopology(
+        mpicomm,
+        brickrange,
+        periodicity = (true, periodic, true),
+        boundary = ((0, 0), (0, 0), (0,0)),
+    )
+    grid = DiscontinuousSpectralElementGrid(
+        topl,
+        FloatType = FT,
+        DeviceArray = Array,
+        polynomialorder = N + Nover,
+    )
+    cflgrid = DiscontinuousSpectralElementGrid(
+        topl,
+        FloatType = FT,
+        DeviceArray = Array,
+        polynomialorder = N,
+    )
+    
+    Δx =  min_node_distance(cflgrid)
+    cfl = 0.1 
+    dt = cfl * Δx / √10
+
+    # run
+    timeend = FT(endtime) # s
+    nout = round(Int, 2 / dt)
+    dt = 2 / nout
+    filename = "3Dflux_" * strip(string(flux), ['(', ')']) * "_p" * string(N) * "_N" * string(Ne) * "_Nover" * string(Nover) * "_periodicity_" * string(periodic)
+    f = jldopen(filename * ".jld2", "a+")
+    f["grid"] = grid
+    close(f)
+    return filename, Ne, dt
+end
+
+function just_generate_name(DOF, N, Nover, flux, periodic; L = 4π, mpicomm = MPI.COMM_WORLD, endtime = 200)
+    Ne = round(Int, DOF / (N+1))
+    Nˣ = Ne
+    Nʸ = Ne
+    Lˣ = L 
+    Lʸ = L
+    filename = "3Dflux_" * strip(string(flux), ['(', ')']) * "_p" * string(N) * "_N" * string(Ne) * "_Nover" * string(Nover) * "_periodicity_" * string(periodic)
+    return filename
+end
+
+function nameprettifier(name)
+    tmp = split(name, "_")
+    dof = "polynomial order " * tmp[3][2] * " and " * tmp[4][2:end] * " elements"
+    stringflux = ", a " * tmp[2] 
+    intorder = ", and integration order 2(" * tmp[3][2] * "+" * tmp[5][end] * ")- 1," 
+    return dof * stringflux * intorder
+end
